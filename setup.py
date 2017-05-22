e1f2d7d93e840c7077215389a54d0a82cafc4bab
Leonardo Gatica <lgatica@protonmail.com>
2017-05-22 23:12:51 +0000

    chore(setup): Update version and update author

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
-#!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='mongogeneric',
-    version='0.1a',
-    description="An implementation of django class based generic views using mongoengine.",
-    author='Jan Schrewe',
-    author_email='jan@schafproductions.com',
-    url='http://www.schafproductions.com',
-    packages=['mongogeneric',],
-    package_data={
-    },
+
+def readme(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='mongogeneric3',
+    version='0.0.2',
+    description=(
+        'An implementation of django class based generic views using '
+        'mongoengine.'),
+    long_description=readme('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='mongo cbv mongoengine',
+    url='http://github.com/lgaticaq/django-mongogeneric',
+    author='Leonardo Gatica',
+    author_email='lgatica@protonmail.com',
+    license='MIT',
+    packages=['mongogeneric'],
+    py_modules=['mongogeneric'],
+    include_package_data=True,
+    zip_safe=False
 )
